cb002dd733228b3d3b62912083c6803e7ca3a3b3
cetincakiroglu <cakiroglucetinn@gmail.com>
2021-02-18 13:49:58 +0300

    add new component

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
-import {Row} from 'react-bootstrap'
+import React,{useRef, useState} from 'react'
+import {Row, Col, Form} from 'react-bootstrap'
 import ArticleCard from './ArticleCard'
 
 function Articles({ saveEdit, formInput , matchedArticle, setid}) {
+    // const checkboxInput = useRef();
+    // const [input,setInput] = useState([]);
+
+    // const handleSubmit = (e) =>{
+    //     const userInput = checkboxInput.current.value;
+    //     e.preventDefault();
+    //     setInput(userInput);
+    //     console.log(input);
+    // }
+
+
     return (
         <>
-            <Row>
-                <ArticleCard 
+            <Row className="gx-0 m-0 p-0 d-flex justify-content-between">
+                {/* <Col id="search__results" md={3} className="bg-success">
+                    results
+                <Form onSubmit={handleSubmit}>
+                    <div key='checkbox' className="mb-3">
+                        <Form.Check type='text'>
+                            <Form.Check.Input isValid />
+                            <Form.Check.Label useRef={checkboxInput}>this is a paragraph</Form.Check.Label>
+                          
+                        </Form.Check>
+                    </div>
+                    <div key='default-checkbox' className="mb-3">
+                        <Form.Check
+                        ref={checkboxInput}
+                        type='checkbox'
+                        id='default-checkbox'
+                        label='this is a search result paragrapgs will be displayed in here'
+                        />
+                    </div>
+                    <div key='default-checkbox' className="mb-3">
+                        <Form.Check
+                        ref={checkboxInput}
+                        type='checkbox'
+                        id='default-checkbox'
+                        label='this is a search result paragrapgs will be displayed in here'
+                        />
+                    </div>
+                    <button type="submit">Show input</button>
+                </Form>
+                </Col> */}
+               
+                
+                <ArticleCard
                 formInput={formInput}
                 matchedArticle = {matchedArticle}
                 saveEdit = {saveEdit}
                 setid = {setid}
                 />
+         
             </Row>
         </>
     )
---
+++ src/Data/dummyData.js
@@ -0,0 +1,8 @@
+const dummyData = [
+
+    "Bitcoin is the hottest thing in the world of money today. It's a virtual currency that has captured the attention of so many people who are very interested to know more about it. It's basically a new way of acquiring money with no central authority. No country or financial institution acts as a middleman in this process which makes this currency secure and easy to use. All you need to do is store your Bitcoins on your computer and generate them using software with a process called 'mining'. ",
+    "Meditation can give you a sense of calm, peace and balance that can benefit both your emotional well-being and your overall health. And these benefits don't end when your meditation session ends. Meditation can help carry you more calmly through your day and may help you manage symptoms of certain medical conditions.",
+    
+  ];
+
+  export default dummyData;
