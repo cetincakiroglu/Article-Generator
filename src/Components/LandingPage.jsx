04a198f323adbc779549626d2d74b5706e83855b
cetincakiroglu <cakiroglucetinn@gmail.com>
2021-02-27 17:07:48 +0300

    Update landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -9,6 +9,8 @@ function LandingPage() {
 //VALUES STATE'INI BURADA BELIRLE, VALUES'U TEKRAR ARAMA YAPTIĞINDA SIFIRLAMAN LAZIM.
 const [values,setValues] = useState([]);
 const [formInput,setFormInput] = useState([]);
+
+//bu state ile eşleşen makaleler gidiyor, bu state'i güncelle
 const [matchedArticle,setMatchedArticle] = useState([]);
 
 
@@ -17,14 +19,18 @@ const [id, setid] = useState(-1);
 const userInput = useRef();
 const history = useHistory();
 //burada formdaki inputtan anahtar kelimeyi alıyoruz
+
 const handleSubmit = (e) =>{
   e.preventDefault();
     const keyword = userInput.current.value;
     console.log('keyword',keyword);
     setFormInput([...formInput, keyword])
     searchArticles(keyword);
+    //fetch yaparsan searchArticles'ı commitle, dummyData'dan veri çekiyor.
   
   }
+
+
 const resetValues = ()=>{
   let array = values;
   if(array[0]){
@@ -36,10 +42,12 @@ const searchArticles = (str) =>{
   if(str == false){//string check
     alert('please enter a valid keyword')
   }else{
+
+    
       let newArticle = dummyData.filter(item => item
         .includes(str.split(' ')[0].toLowerCase() || str.split(' ')[1].toLowerCase()));
         
-      setMatchedArticle([...newArticle]);
+      setMatchedArticle([...newArticle]);//state güncelleme
       console.log('MATCHED ARTICLE', matchedArticle);
    
     }
